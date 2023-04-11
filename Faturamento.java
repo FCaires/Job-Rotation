c7f5fd0e12920953b5a346b19933dc678dec74aa
Fcaires <fc.caires@hotmail.com>
2023-04-11 13:24:03 -0300

    1 commit

--- Faturamento.java
+++ Faturamento.java
@@ -1,9 +1,11 @@
 public class Faturamento {
     public static void main(String[] args) {
-        int[] faturamento = {1000, 2000, 1500, 3000, 2500, 1800, 2200, 1900, 2800, 2300, 2100, 2600, 2400, 2700, 2900, 1700, 3100, 3200, 3300, 3400, 3500, 3600, 3700, 3800, 3900, 4000, 4100, 4200, 4300, 4400, 4500};
+        double[] faturamento = { 22174.1664, 24537.6698, 26139.6134, 0.0, 0.0, 26742.6612, 0.0, 42889.2258, 46251.174,
+                11191.4722, 0.0, 0.0, 3847.4823, 373.7838, 2659.7563, 48924.2448, 18419.2614, 0.0, 0.0, 35240.1826,
+                43829.1667, 18235.6852, 4355.0662, 13327.1025, 0.0, 0.0, 25681.8318, 1718.1221, 13220.495, 13220.495 };
 
         // Menor valor de faturamento
-        int menorFaturamento = faturamento[0];
+        double menorFaturamento = faturamento[0];
         for (int i = 1; i < faturamento.length; i++) {
             if (faturamento[i] < menorFaturamento) {
                 menorFaturamento = faturamento[i];
@@ -12,7 +14,7 @@ public class Faturamento {
         System.out.println("O menor valor de faturamento ocorrido em um dia do mês foi: " + menorFaturamento);
 
         // Maior valor de faturamento
-        int maiorFaturamento = faturamento[0];
+        double maiorFaturamento = faturamento[0];
         for (int i = 1; i < faturamento.length; i++) {
             if (faturamento[i] > maiorFaturamento) {
                 maiorFaturamento = faturamento[i];
@@ -21,19 +23,20 @@ public class Faturamento {
         System.out.println("O maior valor de faturamento ocorrido em um dia do mês foi: " + maiorFaturamento);
 
         // Média mensal de faturamento
-        int somaFaturamento = 0;
-        for (int faturamentoDiario : faturamento) {
+        double somaFaturamento = 0;
+        for (double faturamentoDiario : faturamento) {
             somaFaturamento += faturamentoDiario;
         }
         double mediaMensal = somaFaturamento / (double) faturamento.length;
 
         // Número de dias com faturamento superior à média mensal
         int diasAcimaDaMedia = 0;
-        for (int faturamentoDiario : faturamento) {
+        for (double faturamentoDiario : faturamento) {
             if (faturamentoDiario > mediaMensal) {
                 diasAcimaDaMedia++;
             }
         }
-        System.out.println("O número de dias no mês em que o faturamento diário foi superior à média mensal foi: " + diasAcimaDaMedia);
+        System.out.println("O número de dias no mês em que o faturamento diário foi superior à média mensal foi: "
+                + diasAcimaDaMedia);
     }
 }
